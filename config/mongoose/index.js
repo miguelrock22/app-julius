cd4205d19ed18b5c83a4068ecf3ced8eaa5435d5
miguelrock22 <miguelrock222@gmail.com>
2020-07-02 18:56:37 -0500

    Update mongo conf

--- config/mongoose/index.js
+++ config/mongoose/index.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_CONNECTION, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndexes:true
 }, err => {
-    if (err)
+    if (err){
+        console.log(err)
         throw err;
+    }
     console.log("Mongo is Working");
 });
 
